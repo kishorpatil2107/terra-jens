6791a51315e69219037f06ef1b82e9be5202736d
Ubuntu <ubuntu@ip-172-31-35-240.eu-west-1.compute.internal>
2025-09-20 16:50:51 +0000

    my iac pipeline

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
-    bucket       = "terra-jenkins-state-kishor-2025" # <-- match your actual bucket
+    bucket       = "terra-jenkins-state-kishor-2025"
     key          = "ec2-project/terraform.tfstate"
     region       = "eu-west-1"
     encrypt      = true
     use_lockfile = true
-    profile      = "default"
+    # profile removed so EC2 IAM Role is used
   }
 }
 
--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,8 @@
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type                # Free-tier eligible
-  key_name      = "my-keypair"              # Replace with your existing key pair
 
   tags = {
-    Name = "MyTerraformEC2"
+    Name = "MyTerraEC2"
  }
 }
