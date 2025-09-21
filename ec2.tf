545c9a95d98df8ebd388a44fcdbc6b8c92b48df8
Ubuntu <ubuntu@ip-172-31-47-42.eu-west-1.compute.internal>
2025-09-21 00:11:41 +0000

    made changes in ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
-#webhook auto build
+#webhook
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type                # Free-tier eligible
 
   tags = {
-    Name = "MyTerraEC2"
+    Name = "KishorTerraEC2"
  }
 }
